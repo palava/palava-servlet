4a13365dd56e4322ae326ae34161f30c2a5d63ad
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-04-28 18:54:14 +0000

    added ServletConfig

---
+++ src/main/java/de/cosmocode/palava/servlet/ServletConfig.java
@@ -0,0 +1,16 @@
+package de.cosmocode.palava.servlet;
+
+/**
+ * Static constant holder class for servlet config key names.
+ *
+ * @author Willi Schoenborn
+ */
+public final class ServletConfig {
+
+    public static final String PREFIX = "servlet.";
+    
+    private ServletConfig() {
+        
+    }
+
+}
