8dde9e70aeab3e99e23e533a1002ff3cec7a7897
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-04-29 20:28:19 +0000

    added di to echo servlet

--- src/test/java/de/cosmocode/palava/servlet/EchoServlet.java
+++ src/test/java/de/cosmocode/palava/servlet/EchoServlet.java
@@ -22,6 +22,7 @@ package de.cosmocode.palava.servlet;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Arrays;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,7 +31,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+
+import de.cosmocode.palava.core.Registry;
 
 /**
  * Test servlet.
@@ -44,14 +48,17 @@ public final class EchoServlet extends HttpServlet {
 
     private static final Logger LOG = LoggerFactory.getLogger(EchoServlet.class);
     
+    @Inject
+    public EchoServlet(Registry registry) {
+        LOG.info("Injected registry", registry);
+    }
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         LOG.info("Incoming request on {}", this);
         final Writer writer = response.getWriter();
-        writer.write(request.getParameterMap().toString());
+        writer.write(Arrays.toString(Object[].class.cast(request.getParameterMap().get("q"))));
         writer.flush();
-        writer.close();
     }
-    
 
 }
